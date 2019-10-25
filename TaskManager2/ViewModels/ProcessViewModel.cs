c5a1e090cadfba72837717ad952214b04b330ae3
MindF <MindFlare56@hotmail.fr>
2019-10-25 16:12:11 -0400

    Add project files.

---
+++ TaskManager2/ViewModels/ProcessViewModel.cs
@@ -0,0 +1,28 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace TaskManager2.ViewModels
+{
+    class ProcessViewModel : Screen
+    {
+
+        public ProcessViewModel()
+        {
+            var list = new List<string>();
+            list.Add("bob");
+            list.Add("bob2");
+            list.Add("bob3");
+            GridData = new ListCollectionView(list);
+        }
+
+        ListCollectionView GridData
+        {
+            get; set;
+        }
+    }
+}
---
+++ TaskManager2/ViewModels/TaskManagerViewModel.cs
@@ -0,0 +1,27 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManager2.ViewModels
+{
+    public class TaskManagerViewModel : Conductor<object>
+    {
+        public TaskManagerViewModel()
+        {
+
+        }
+
+        public void ProcessButton()
+        {
+            ActivateItem(new ProcessViewModel());
+        }
+
+        public void RuleButton()
+        {
+            ActivateItem(new RuleViewModel());
+        }
+    }   
+}
